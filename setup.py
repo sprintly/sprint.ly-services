d0e918ee038345215389046bff15839158e658e3
Adrian Perez <adrianperez.deb@gmail.com>
2013-09-08 16:29:06 +0200

    Fix missing declaration for 'requests' module

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     license='BSD',
     packages=find_packages(),
     zip_safe=False,
-    install_requires=['Django', 'pinder', 'simplejson'],
+    install_requires=['Django', 'pinder', 'simplejson', 'requests'],
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python",
