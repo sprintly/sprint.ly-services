9e7d2e8a6f175229b618a0d6d3a4aab0d895a2d6
Schuyler Erle <schuyler@nocat.net>
2013-10-03 12:12:30 -0700

    Add a service for slack.com. Requires the same params as webhooks.py.

---
+++ lookout/services/slack.py
@@ -0,0 +1,13 @@
+from .webhook import Service as WebHookService
+from lookout.base import MessageServiceBase
+
+class Service(WebHookService, MessageServiceBase):
+    """
+    Slack
+
+    This will send item events from Sprint.ly to Slack. Simply configure an
+    incoming WebHook for your Slack account here.
+    """
+    def send(self, payload):
+        payload['text'] = self.message(payload)
+        super(WebHookService, self).send(payload)
