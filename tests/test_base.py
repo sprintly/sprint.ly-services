aec20c5383f5812ea3c89b131a5e8a60627d2413
Chris Forrette <chris@chrisforrette.com>
2014-02-21 17:19:27 -0800

    Adding a test for _clean_mentions

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-from lookout.base import get_available_services
+from lookout.base import get_available_services, MessageServiceBase
 
 
 ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
@@ -12,3 +12,8 @@ def test_get_available_services():
     services_dir = os.path.join(ROOT_DIR, 'lookout', 'services')
     service_file_list = filter(lambda service_file: not re.match('.*\.pyc$', service_file) and not re.match('^__', service_file), os.listdir(services_dir))
     assert len(service_file_list) == len(services)
+
+def test_clean_mentions():
+    service = MessageServiceBase({})
+    comment = "Hello there @[Joe Stump](pk:1)"
+    assert service._clean_mentions(comment) == "Hello there Joe Stump"
