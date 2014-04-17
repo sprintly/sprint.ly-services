3fb859bb63605f577a11e539b80658065f0bd213
Chris Forrette <chris@chrisforrette.com>
2014-04-16 18:17:21 -0700

    Parameterizing some of the slack tests to run them on all payload types

--- tests/test_slack.py
+++ tests/test_slack.py
@@ -1,8 +1,9 @@
 from mock import patch
+import pytest
 
 from lookout.services.slack import Service
 
-from .fixtures import fake_item_payload
+from .fixtures import all_payloads, fake_item_payload
 
 
 def test_get_attachment_color():
@@ -13,16 +14,21 @@ def test_get_attachment_color_empty():
     srv = Service({})
     assert type(srv.get_attachment_color()) == str
 
-def test_get_post_data():
+@pytest.mark.parametrize('payload', all_payloads)
+def test_get_post_data(payload):
+    model = payload['model'].lower()
     srv = Service({})
-    with patch.object(srv, 'get_item_attachment') as mock_get_item_attachment:
-        mock_get_item_attachment.return_value = {}
-        post_data = srv.get_post_data(fake_item_payload)
+    with patch.object(srv, 'get_%s_attachment' % model) as mock_get_attachment:
+        mock_get_attachment.return_value = {}
+        post_data = srv.get_post_data(payload)
         assert post_data.has_key('attachments')
-        mock_get_item_attachment.assert_called_with(fake_item_payload)
+        mock_get_attachment.assert_called_with(payload)
 
-def test_get_item_attachment():
+@pytest.mark.parametrize('payload', all_payloads)
+def test_get_attachment(payload):
     srv = Service({})
-    attachment = srv.get_item_attachment(fake_item_payload)
+    model = payload['model'].lower()
+    method = 'get_%s_attachment' % model
+    attachment = getattr(srv, method)(payload)
     assert type(attachment) == dict
     assert attachment.has_key('text')
